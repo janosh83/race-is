cd394accfc835ce352d3dfbce61bc66f73998ea9
Zdeněk Jančík <zdenek.jancik@gmail.com>
2020-03-22 17:05:48 +0100

    Added code to display one peak at page peak/$id

---
+++ src/Controller/PeakController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Peak;
+
+class PeakController extends AbstractController
+{
+    /**
+     * @Route("/peak/{id}", name="peak_show")
+     */
+    public function show($id)
+    {
+        $peak = $this->getDoctrine()
+            ->getRepository(Peak::class)
+            ->find($id);
+
+        if (!$peak) {
+            throw $this->createNotFoundException(
+                'Peak not found '.$id
+            );
+        }
+
+        // or render a template
+        // in the template, print things with {{ product.name }}
+        return $this->render('peak/index.html.twig', ['peak' => $peak]);
+    }
+}
---
+++ src/Entity/Peak.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PeakRepository")
+ */
+class Peak
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=32)
+     */
+    private $short_id;
+
+    /**
+     * @ORM\Column(type="string", length=128)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private $latitude;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private $longitude;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getShortId(): ?string
+    {
+        return $this->short_id;
+    }
+
+    public function setShortId(string $short_id): self
+    {
+        $this->short_id = $short_id;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getLatitude(): ?float
+    {
+        return $this->latitude;
+    }
+
+    public function setLatitude(?float $latitude): self
+    {
+        $this->latitude = $latitude;
+
+        return $this;
+    }
+
+    public function getLongitude(): ?float
+    {
+        return $this->longitude;
+    }
+
+    public function setLongitude(?float $longitude): self
+    {
+        $this->longitude = $longitude;
+
+        return $this;
+    }
+}
